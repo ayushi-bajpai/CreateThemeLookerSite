5833ed94bfd04f8b1ea0e66955ec6f9d1b67c577
ThePiGirl <ayushi.bajpai1111@gmail.com>
2019-08-02 00:24:51 +0530

    re

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.menu-toggle').click(function () {
+                $('nav').toggleClass('active');
+            })
+        })
+    </script>
