5db98a1562aa3d22f48e4addc714e419846ea69f
ThePiGirl <ayushi.bajpai1111@gmail.com>
2019-08-04 19:43:52 +0530

    nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,7 @@
-<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $('.menu-toggle').click(function () {
-                $('nav').toggleClass('active');
-            })
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.menu-toggle').click(function () {
+            $('nav').toggleClass('active');
         })
-    </script>
+    })
+</script>
